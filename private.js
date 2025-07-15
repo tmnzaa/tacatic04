0894879f0107d4c9b20986eca115833be12a4e40
tmnzaa <polosanpes818@gmail.com>
2025-07-16 00:25:34 +0700

    Update private.js

--- private.js
+++ private.js
@@ -3,7 +3,7 @@ const path = './data_user.json'
 if (!fs.existsSync(path)) fs.writeJsonSync(path, {})
 
 // 🎩 Nomor Owner Bot
-const OWNER_NUM = '6285179690350'
+const OWNER_NUM = '62895379065009'
 
 module.exports = async (sock, msg) => {
   const from = msg.key.remoteJid
